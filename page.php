372a44d215edf67a0afbda8e79798bb5ba99d7c6
Vichea <mr.vichea.007@gmail.com>
2020-01-15 21:20:39 +0700

    a

---
+++ page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php include('html/template/header.html') ?>
+
+    <section class="banner_part">
+        <div class="single_banner_slider">
+            <div class="banner_text banner_text2">
+                
+            </div>
+        </div>
+    </section>
+    
+    <div class="fake_container">
+        superblah
+    </div>
+
+<?php include('html/template/footer.html') ?>
+</body>
+</html>
