023a6af365104c486931bcf639a05639293890e8
Cheakimse Mao <cheakimsemao@Cheakimse-Mao.local>
2020-01-17 21:50:54 +0700

    a

--- contact.php
+++ contact.php
@@ -6,33 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/contact.css">
 </head>
 
 <body>
     <?php include('html/template/header.html') ?>
 
-    <!-- code here -->
+    <section class="banner_part">
+        <div class="single_banner_slider">
+            <div class="banner_text banner_text2">
 
-    <!-- contact background -->
-    <div class="contact-background">
-        <!-- Home - Contact us -->
-        <!-- background image -->
-    </div>
-    <!-- map -->
-    <div class="ministry-location">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.689555183988!2d104.9205208149457!3d11.574098447127454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095144f3bfe905%3A0xa9a18d986f6c66b0!2z4Z6A4Z-S4Z6a4Z6f4Z694Z6E4Z6f4Z624Z6S4Z624Z6a4Z6O4Z6A4Z624Z6aIOGek-Get-GehOGeiuGeueGegOGeh-GeieGfkuGeh-GevOGekw!5e0!3m2!1skm!2skh!4v1579245530516!5m2!1skm!2skh" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-    </div>
-    <!-- get in touch -->
-    <div>
+            </div>
+        </div>
+    </section>
 
-    </div>
-    <!-- more - get in touch -->
-    <div>
-
-    </div>
-    <!-- others -->
-    <div>
-        
+    <div class="fake_container">
+        <div class="contact_background">
+            <div class="contact_text">
+                <h1>Contact</h1>
+            </div>
+        </div>
+        <br><br>
+        <div class="contact_form">
+            <h3>Get in touch</h3>
+        </div>
+        <div class="ministry_location">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2369.7601504738864!2d104.92223408648967!3d11.575880604749903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095144f3bfe905%3A0xa9a18d986f6c66b0!2z4Z6A4Z-S4Z6a4Z6f4Z694Z6E4Z6f4Z624Z6S4Z624Z6a4Z6O4Z6A4Z624Z6aIOGek-Get-GehOGeiuGeueGegOGeh-GeieGfkuGeh-GevOGekw!5e0!3m2!1skm!2skh!4v1579270944831!5m2!1skm!2skh" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
     </div>
 
     <?php include('html/template/footer.html') ?>
