17bcb4ccdd33f81578ccf2db11f5ab606a43e655
Cheakimse Mao <cheakimsemao@Cheakimse-Mao.local>
2020-01-20 22:49:13 +0700

    a

--- contact.php
+++ contact.php
@@ -6,7 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="css/contact.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/contact.css">
 </head>
 
 <body>
@@ -14,25 +26,154 @@
 
     <section class="banner_part">
         <div class="single_banner_slider">
-            <div class="banner_text banner_text2">
-
-            </div>
+            <div class="banner_text banner_text2"></div>
         </div>
     </section>
 
     <div class="fake_container">
-        <div class="contact_background">
-            <div class="contact_text">
-                <h1>Contact</h1>
+        
+        <div class="container-contact100">
+            <div class="wrap-contact100">
+                <form class="contact100-form validate-form">
+                    <span class="contact100-form-title">
+                        Send Us A Message
+                    </span>
+
+                    <label class="label-input100" for="first-name">Tell us your name *</label>
+                    <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Type first name">
+                        <input id="first-name" class="input100" type="text" name="first-name" placeholder="First name">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <div class="wrap-input100 rs2-wrap-input100 validate-input" data-validate="Type last name">
+                        <input class="input100" type="text" name="last-name" placeholder="Last name">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <label class="label-input100" for="first-name">Your organization and position *</label>
+                    <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="">
+                        <input id="organization" class="input100" type="text" name="organization" placeholder="Organization">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <div class="wrap-input100 rs2-wrap-input100 validate-input" data-validate="">
+                        <input class="input100" type="text" name="position" placeholder="Position">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <label class="label-input100" for="email">Enter your email *</label>
+                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                        <input id="email" class="input100" type="text" name="email" placeholder="Eg. example@email.com">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <label class="label-input100" for="phone">Enter phone number</label>
+                    <div class="wrap-input100">
+                        <input id="phone" class="input100" type="text" name="phone" placeholder="Eg. +8550000000000">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <label class="label-input100" for="message">Message *</label>
+                    <div class="wrap-input100 validate-input" data-validate="Message is required">
+                        <textarea id="message" class="input100" name="message" placeholder="Write us a message"></textarea>
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <div class="container-contact100-form-btn">
+                        <button class="contact100-form-btn">
+                            Send Message
+                        </button>
+                    </div>
+                </form>
+                <!-- contact info -->
+                <div class="contact100-more flex-col-c-m">
+                    <span class="contact100-form-title">
+                        Contact Info
+                    </span>
+
+                    <div class="flex-w size1 p-b-47">
+                        <div class="txt1 p-r-25">
+                            <span class="lnr lnr-map-marker"></span>
+                        </div>
+
+                        <div class="flex-col size2">
+                            <span class="txt1 p-b-20">
+                                Address
+                            </span>
+
+                            <span class="txt2">
+                                Corner of Preah Norodom Blvd and Street 106, Phnom Penh, Cambodia.
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="dis-flex size1 p-b-47">
+                        <div class="txt1 p-r-25">
+                            <span class="lnr lnr-phone-handset"></span>
+                        </div>
+
+                        <div class="flex-col size2">
+                            <span class="txt1 p-b-20">
+                                Phone Number
+                            </span>
+
+                            <span class="txt2">
+                                (+855) (085 | 015 | 067) 92 90 90
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="dis-flex size1 p-b-47">
+                        <div class="txt1 p-r-25">
+                            <span class="lnr lnr-envelope"></span>
+                        </div>
+
+                        <div class="flex-col size2">
+                            <span class="txt1 p-b-20">
+                                Email
+                            </span>
+
+                            <span class="txt2">
+                                info@mpwt.gov.kh
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="ministry-location">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.689555183988!2d104.9205208149457!3d11.574098447127454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095144f3bfe905%3A0xa9a18d986f6c66b0!2z4Z6A4Z-S4Z6a4Z6f4Z694Z6E4Z6f4Z624Z6S4Z624Z6a4Z6O4Z6A4Z624Z6aIOGek-Get-GehOGeiuGeueGegOGeh-GeieGfkuGeh-GevOGekw!5e0!3m2!1skm!2skh!4v1579504283077!5m2!1skm!2skh" width="450" height="250px" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    </div>
+                </div>
             </div>
         </div>
-        <br><br>
-        <div class="contact_form">
-            <h3>Get in touch</h3>
-        </div>
-        <div class="ministry_location">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2369.7601504738864!2d104.92223408648967!3d11.575880604749903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095144f3bfe905%3A0xa9a18d986f6c66b0!2z4Z6A4Z-S4Z6a4Z6f4Z694Z6E4Z6f4Z624Z6S4Z624Z6a4Z6O4Z6A4Z624Z6aIOGek-Get-GehOGeiuGeueGegOGeh-GeieGfkuGeh-GevOGekw!5e0!3m2!1skm!2skh!4v1579270944831!5m2!1skm!2skh" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-        </div>
+
+        <div id="dropDownSelect1"></div>
+
+        <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+        <script src="vendor/animsition/js/animsition.min.js"></script>
+        <script src="vendor/bootstrap/js/popper.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="vendor/select2/select2.min.js"></script>
+        <script>
+            $(".selection-2").select2({
+                minimumResultsForSearch: 20,
+                dropdownParent: $('#dropDownSelect1')
+            });
+        </script>
+        <script src="vendor/daterangepicker/moment.min.js"></script>
+        <script src="vendor/daterangepicker/daterangepicker.js"></script>
+        <script src="vendor/countdowntime/countdowntime.js"></script>
+        <script src="js/main.js"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-23581568-13');
+        </script>
+
     </div>
 
     <?php include('html/template/footer.html') ?>
