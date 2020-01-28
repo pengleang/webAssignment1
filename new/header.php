32e9264dc5787039219c72ae42c025db7a3343cb
Vichea <mr.vichea.007@gmail.com>
2020-01-28 16:24:30 +0700

    a

---
+++ new/header.php
@@ -0,0 +1,204 @@
+<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700|Oswald:400,700" rel="stylesheet">
+
+<link rel="stylesheet" href="fonts/icomoon/style.css">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/jquery.fancybox.min.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+<link rel="stylesheet" href="css/aos.css">
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<style>
+  .blah-contact{
+    min-width:200px !important;
+  }
+</style>
+
+<div id="overlayer"></div>
+  <div class="loader">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+  </div>
+</div>
+  
+  <div class="site-wrap"  id="home-section">
+
+    <!-- <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> -->
+
+    <div class="top-bar">
+      <div>
+        <img src="images/page_logo.jpg" alt="" class="page_logo">
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <a href="#" class="text-white"><span class="mr-2 text-white icon-envelope-open-o"></span> <span class="d-none d-md-inline-block">info@yourdomain.com</span></a>
+            <span class="mx-md-2 d-inline-block"></span>
+            <a href="#" class="text-white"><span class="mr-2 text-white icon-phone"></span> <span class="d-none d-md-inline-block">1+ (234) 5678 9101</span></a>
+            
+
+            <div class="float-right">
+
+            <a href="#" class="text-white"><span class="mr-2 text-white icon-twitter"></span> <span class="d-none d-md-inline-block">Twitter</span></a>
+            <span class="mx-md-2 d-inline-block"></span>
+            <a href="#" class="text-white"><span class="mr-2 text-white icon-instagram"></span> <span class="d-none d-md-inline-block">Instagram</span></a>
+
+            </div>
+
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+      
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center position-relative">
+          
+            
+            
+            
+            <!-- <div class="col-12"> -->
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                <li><a href="#home-section" class="nav-link">Home</a></li>
+              
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">Department</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#team-section" class="nav-link">Department of Logistics Information</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Pricing</a></li>
+                    <li class="has-children">
+                      <a href="#">Department of Logistics</a>
+                      <ul class="dropdown">
+                        <li><a href="#">Letter from His Excellency</a></li>
+                        <li><a href="#">History of His Excellency</a></li>
+                        <li><a href="#">Vision and Mission</a></li>
+                        <li><a href="#">Role and Responsibility</a></li>
+                        <li><a href="#">Organization Chart</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#faq-section" class="nav-link">Department of Logistics Cooperation</a></li>
+                    <li><a href="#faq-section" class="nav-link">Department of Monitoring and Evaluation</a></li>
+                  </ul>
+                </li>
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">Laws and Policy</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#team-section" class="nav-link">Laws</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Loyal Decree</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Sub-Decree</a></li>
+                    <li><a href="#pricing-section" class="nav-link">ស.ជ.ណ</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Decision/Prokas</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Official Paper</a></li>
+                  </ul>
+                </li>
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">Logistics</a>
+                  <ul class="dropdown arrow-top">
+                    <li class="has-children">
+                      <a href="#">Logistics Project and Priority</a>
+                      <ul class="dropdown">
+                        <li><a href="#team-section" >List of Priority Projects in Logistics</a></li>
+                        <li><a href="#pricing-section" >Strategies, Programs, and Projects for Enhancing Logistics Service Providers</a></li>
+                        <li><a href="#pricing-section" >Capacity Building Activity Framework</a></li>
+                        <li><a href="#pricing-section" >Work On Capcity Building Activities During Trasition</a></li>
+                      </ul>
+                    </li>
+                    <li class="has-children">
+                      <a href="#">Logistics Center</a>
+                      <ul class="dropdown">
+                        <li><a href="#team-section" >Training and Research Center of Logistics</a></li>
+                        <li><a href="#pricing-section" >PP Complex Logistics Center</a></li>
+                        <li><a href="#pricing-section" >Sihanoukvile Complex Logistics Center</a></li>
+                        <li><a href="#pricing-section" >Capacity Building and Institutional Strengthening for Logistics Monitoring and Evaluation For CLV Countries</a></li>
+                        <li><a href="#pricing-section" >Logistics Database Center</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">System Management</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#team-section" class="nav-link">Track and Trace System</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Operational Logistics Information</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Operational Logistics Complex Center</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Information Maps and Road or Logistic Capacity Assessment</a></li>
+                  </ul>
+                </li>
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">International Relations</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#team-section" class="nav-link">Development Partners</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Master Plan</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Purchase Power Parity (PPP)</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Country Partners</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Memorandum of Unserstanding (MoU) </a></li>
+                  </ul>
+                </li>
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">More Information</a>
+                  <ul class="dropdown arrow-top">
+                    <li class="has-children">
+                      <a href="#">Report</a>
+                      <ul class="dropdown blah">
+                        <li><a href="#">Meeting of Leaders of Department of Logistics</a></li>
+                        <li><a href="#">Department of Logistics Information</a></li>
+                        <li><a href="#">Department of Logistics</a></li>
+                        <li><a href="#">Department of Logistics Cooperation</a></li>
+                        <li><a href="#">Department of Monitoring and Evaluation</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#faq-section" class="nav-link">Hot News</a></li>
+                    <li><a href="#faq-section" class="nav-link">Technical Cooperation</a></li>
+                    <li><a href="#faq-section" class="nav-link">Training Course</a></li>
+                    <li><a href="#faq-section" class="nav-link">Seminar</a></li>
+                    <li><a href="#faq-section" class="nav-link">Job Opportunity</a></li>
+                  </ul>
+                </li>
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">Contact and Help</a>
+                  <ul class="dropdown arrow-top">
+                    <li class="blah-contact"><a href="#team-section" class="nav-link">Contact</a></li>
+                    <li class="blah-contact"><a href="#pricing-section" class="nav-link">Social Media</a></li>
+                  </ul>
+                </li>
+
+              </ul>
+              </nav>
+          
+            <!-- </div> -->
+
+          <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+    
+    
+
+
+
+    
---
+++ new/index.php
@@ -0,0 +1,69 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php include("header.php"); ?>
+  <div class="owl-carousel slide-one-item">    
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/banner1.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-7">
+              <h1 data-aos="fade-up" data-aos-delay="">Welcome to UnEarth</h1>  
+              <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, cumque vitae animi.</p>
+              <p data-aos="fade-up" data-aos-delay="200"><a href="#" class="btn btn-outline-white border-w-2 btn-md">Get in touch</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/banner2.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-8">
+              <h1 data-aos="fade-up" data-aos-delay="">New Generation of Mining</h1>      
+              <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est odit dolorum voluptates!</p>
+              <p data-aos="fade-up" data-aos-delay="200"><a href="#" class="btn btn-outline-white border-w-2 btn-md">Get in touch</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/banner3.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-8">
+              <h1 data-aos="fade-up" data-aos-delay="">New Generation of Mining</h1>      
+              <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est odit dolorum voluptates!</p>
+              <p data-aos="fade-up" data-aos-delay="200"><a href="#" class="btn btn-outline-white border-w-2 btn-md">Get in touch</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/banner4.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-8">
+              <h1 data-aos="fade-up" data-aos-delay="">New Generation of Mining</h1>      
+              <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est odit dolorum voluptates!</p>
+              <p data-aos="fade-up" data-aos-delay="200"><a href="#" class="btn btn-outline-white border-w-2 btn-md">Get in touch</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+
+    <div class="fake-container">
+      <p>super blah</p>
+    </div>
+
+    <?php include("footer.php"); ?>
+</body>
+</html>
